b317f735598fb3329b267894f479d9d6d1662fe4
Asiaaa <joanna.wolna@ymail.com>
2015-01-02 13:43:31 +0100

    Paczki

--- src/Model/Kontrahent.java
+++ src/Model/Kontrahent.java
@@ -10,6 +10,65 @@ package Model;
  *
  * @author Joanna
  */
-public class Kontrahent {
+public class Kontrahent extends Uzytkownik{
+    private String nazwa_firmy;
+    private int nip;
     
-}
+    
+    public String get_nazwa_firmy()
+    {
+        return this.nazwa_firmy;
+    }
+    public void set_nazwa_firmy(String nnazwa_firmy)
+    {
+        this.nazwa_firmy=nnazwa_firmy;
+    }
+    public int get_nip()
+    {
+        return this.nip;
+    }
+    public void set_nip(int nnip)
+    {
+        this.nip=nnip;
+    }
+    public Kontrahent DodajKontrahenta(String iimie, String nnazwisko, String ttelefon, String eemail, String mmiejscowosc,
+            String uulica, String nnazwa_firmy, int nnip)
+    {
+        Kontrahent nowy = new Kontrahent();
+        if(iimie.length()>0)
+            nowy.set_imie(iimie);
+        else
+            nowy.set_imie(null);
+        if(nnazwisko.length()>0)
+            nowy.set_nazwisko(nnazwisko);
+        else
+            nowy.set_nazwisko(null);
+        if(ttelefon.length()>0)
+             nowy.set_telefon(ttelefon);
+        else 
+            nowy.set_telefon(null);
+        if(eemail.length()>0)
+            nowy.set_email(eemail);
+        else 
+            nowy.set_email(null);
+        if(mmiejscowosc.length()>0)
+            nowy.set_miejscowosc(mmiejscowosc);
+        else
+            nowy.set_miejscowosc(null);
+        if(uulica.length()>0)
+            nowy.set_ulica(uulica);
+        else
+            nowy.set_ulica(null);
+        if(nnazwa_firmy.length()>0)
+            nowy.set_nazwa_firmy(nnazwa_firmy);
+        else
+            nowy.set_nazwa_firmy(null);
+        if(nnip!=0)
+            nowy.set_nip(nnip);
+        else 
+            nowy.set_nip(0);
+        
+        return nowy;
+    }
+ }
+
---
+++ src/Model/Uzytkownik.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Joanna
+ */
+public class Uzytkownik {
+    private String imie;
+    private String nazwisko;
+    private String telefon;
+    private String email;
+    private String miejscowosc;
+    private String ulica;
+    
+    public String get_imie()
+    {
+    return this.imie;
+    }
+    public void set_imie(String iimie)
+    {
+        this.imie=iimie;
+    }
+    public String get_nazwisko()
+    {
+        return this.nazwisko;
+    }
+    public void set_nazwisko(String nnazwisko)
+    {
+        this.nazwisko=nnazwisko;
+    }
+    public String get_telefon()
+    {
+        return this.telefon;
+    }
+    public void set_telefon(String ttelefon)
+    {
+        this.telefon=ttelefon;
+    }
+    public String get_email()
+    {
+        return this.telefon;
+    }
+    public void set_email(String eemail)
+    {
+        this.email=eemail;
+    }
+    public String get_miejscowosc()
+    {
+        return this.miejscowosc;
+    }
+    public void set_miejscowosc(String mmiejscowosc)
+    {
+        this.miejscowosc=mmiejscowosc;
+    }
+    public String get_ulica()
+    {
+        return this.ulica;
+    }
+    public void set_ulica(String uulica)
+    {
+        this.ulica=uulica;
+    }
+    
+}
